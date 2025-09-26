d7914c9be51a8959b89d116f38486bc2251da644
Mujtaba Qureshi <mujtabaqureshi009@gmail.com>
2025-09-26 03:30:47 -0700

    Task5.cpp

---
+++ 24k-0535-Lab05/Task5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumDigits(int n) {
+    if (n == 0) return 0;
+    return n % 10 + sumDigits(n / 10);
+}
+
+int nestedSum(int n) {
+    if (n < 10) return n;
+    return nestedSum(sumDigits(n));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << nestedSum(n) << endl;
+    return 0;
+}
